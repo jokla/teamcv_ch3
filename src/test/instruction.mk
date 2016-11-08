0010a2d49c1041adfec5c9a304d7f042328dc240
jokla <giovanni.claudio@inria.fr>
2016-11-08 17:12:23 +0100

    Added CMakeLists.txt to build test and instructions

---
+++ src/test/instruction.mk
@@ -0,0 +1,5 @@
+$ ./test_train_bow -d=/home/jokla/catkin_ws/src/challenge2/src/test/pic/ --detector SURF --extractor SURF
+
+
+$ ./test_detect_bow -d=/home/jokla/catkin_ws/src/challenge2/data/center --train_dir=/home/jokla/catkin_ws/src/challenge2/src/test/pic_elcamino/
+
