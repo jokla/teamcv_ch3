812558ff2ffc3c355799588b45fa16163001d867
jokla <giovanni.claudio@inria.fr>
2016-11-08 17:25:57 +0100

    Update instructions

--- src/test/instruction.mk
+++ src/test/instruction.mk
@@ -1,5 +1,4 @@
-$ ./test_train_bow -d=/home/jokla/catkin_ws/src/challenge2/src/test/pic/ --detector SURF --extractor SURF
-
+$ ./test_train_bow -d=/home/jokla/catkin_ws/src/challenge2/src/test/pic/ 
 
 $ ./test_detect_bow -d=/home/jokla/catkin_ws/src/challenge2/data/center --train_dir=/home/jokla/catkin_ws/src/challenge2/src/test/pic_elcamino/
 
